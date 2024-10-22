e749469253dda937dbaad076857d2b6151e58fb6
booboox <c79209811@gmail.com>
2024-10-22 09:03:05 +0800

    Update index.vue
    添加轮播图，美化页面

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,11 @@
 <script setup>
 import { newMusic, songPlaylist } from "@/api/api";
+import { ElCarousel, ElCarouselItem } from 'element-plus';
+import img1 from '@/static/img/lbt/1.png';
+import img2 from '@/static/img/lbt/2.png';
+import img3 from '@/static/img/lbt/3.png';
+import img4 from '@/static/img/lbt/4.png';
+
 const router = useRouter();
 const state = reactive({
   searchKw: "",
@@ -7,6 +13,10 @@ const state = reactive({
   playlists: [],
   loading: true,
 });
+
+// 导入的图片数组
+const carouselImages = [img1, img2, img3, img4];
+
 const { tableData, playlists, loading } = toRefs(state);
 
 onMounted(async () => {
@@ -34,41 +44,16 @@ onMounted(async () => {
   });
 });
 </script>
+
 <template>
   <div>
-    <div class="content-wrapper-header">
-      <div class="content-wrapper-context">
-        <h3 class="img-content">
-          <svg
-            t="1648788969161"
-            class="icon"
-            viewBox="0 0 1024 1024"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            p-id="5991"
-            width="25"
-            height="25"
-          >
-            <path
-              d="M1006.652621 469.673144l-114.751779-114.751778V192.356347a60.318243 60.318243 0 0 0-60.318242-59.950449H669.385374L554.633596 17.65412a59.950448 59.950448 0 0 0-84.960452 0L354.921366 132.405898H192.356347A59.950448 59.950448 0 0 0 132.405898 192.356347v162.565019L17.65412 469.673144a59.950448 59.950448 0 0 0 0 84.960452L132.405898 669.385374v162.197226a59.950448 59.950448 0 0 0 59.950449 59.950448h162.565019l114.751778 115.119573a60.686037 60.686037 0 0 0 84.960452 0l114.751778-115.119573h162.197226a60.318243 60.318243 0 0 0 60.318242-59.950448V669.385374l114.751779-114.751778a60.686037 60.686037 0 0 0 0-84.960452z m-36.779417 49.652212l-116.590749 116.59075a44.503094 44.503094 0 0 0-12.872796 31.262503v164.403991a10.666031 10.666031 0 0 1-10.66603 10.66603h-162.56502a43.767505 43.767505 0 0 0-31.262503 12.872796l-116.222956 116.222955a10.298237 10.298237 0 0 1-15.07956 0l-116.222955-116.222955a43.767505 43.767505 0 0 0-31.262504-12.872796H192.356347a10.666031 10.666031 0 0 1-10.298237-10.66603v-164.403991a43.399711 43.399711 0 0 0-13.24059-31.262503l-116.222955-116.59075a10.298237 10.298237 0 0 1 0-14.711766l116.222955-116.222955a44.135299 44.135299 0 0 0 13.24059-31.630298V192.356347A10.298237 10.298237 0 0 1 192.356347 183.897081h164.771784a44.135299 44.135299 0 0 0 31.262504-13.24059l118.061926-118.061926a11.033825 11.033825 0 0 1 7.355883-2.942353 11.401619 11.401619 0 0 1 7.723677 2.942353l116.222955 116.222955a44.135299 44.135299 0 0 0 31.262504 13.24059h164.40399a10.298237 10.298237 0 0 1 10.666031 10.298237V356.760337a45.606476 45.606476 0 0 0 12.872796 31.630298l116.590749 116.222955a10.298237 10.298237 0 0 1-1.838971 14.711766z"
-              p-id="5992"
-              fill="#1296db"
-            ></path>
-            <path  
-              d="M693.659789 331.014746l-187.207228 274.374444-141.232958-110.338248c-16.182943 0-24.642209 8.459266-24.642209 26.848973l176.173403 159.254872 207.803702-331.014745c5.516912-9.930442-23.538826-31.262504-30.89471-19.125296z"
-              p-id="5993"
-              fill="#67C23A"
-            ></path>
-          </svg >
-          《找自己》
-        </h3>
-        <div class="content-text">
-          是一首由陶喆 作词、作曲并演唱的 国语流行音乐,收录于专辑《I'm ok》,发行公司为 金牌大风 。 在专辑《I'm ok》中,陶喆以摇滚的精神作为音乐背景,从第一首歌《找自己》开始就摆脱了上一张r&b的风格,以alternat ive rock的形式呈现。
-          并在hook部分表达了陶喆想带粉丝去“tao B”的强烈心情
-        </div>
-      </div>
-     
-    </div>
+    <!-- 添加轮播图 -->
+    <el-carousel :interval="4000" type="card" height="200px">
+      <el-carousel-item v-for="(image, index) in carouselImages" :key="index">
+        <img :src="image" alt="Carousel Image" style="width: 100%; height: 100%; object-fit: cover;">
+      </el-carousel-item>
+    </el-carousel>
+
     <el-skeleton :rows="5" animated :loading="loading">
       <template #default>
         <div class="content-section">
@@ -96,4 +81,27 @@ onMounted(async () => {
     </el-skeleton>
   </div>
 </template>
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+.el-carousel__item img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover; /* 确保图片填满 */
+}
+
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
